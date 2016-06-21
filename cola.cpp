f270c21a89770761878df89bf204e1ba95169b8e
bleico <bleiderc@gmail.com>
2016-06-21 15:04:54 -0400

    iniciar cola

--- cola.cpp
+++ cola.cpp
@@ -7,24 +7,42 @@ struct registro
 {
        int valor;
        struct registro *sig;       
-}temp;
+};
 
 typedef registro nodo ;
 
-void iniciarCola (  nodo *primero , nodo *nuevo , nodo *ultimo)
-{
-	nuevo = new (nodo);	
-
-	
-}
-
 bool colaVacia (nodo *inicio)
 {
-	if (inicio ==NULL)
+	if (inicio == NULL)
+	{
+		void iniciarCola();
 		return true;
+	}
+
 	else
 		return false;
 
+}
+
+void iniciarCola (  nodo *primero , nodo *nuevo , nodo *ultimo, int v)
+{
+	if (primero == NULL)
+	{
+		nuevo = new (nodo);	
+		nuevo -> valor = v;
+		nuevo = primero ;
+		nuevo->sig =NULL;
+	}
+	else
+	{
+		nuevo = new (nodo);	
+		nuevo -> valor = v;
+		nuevo = ultimo ;
+		nuevo->sig =NULL;
+
+		cout <<"hola "<<endl;
+		system("Pause");
+	}
 }
 
 void encolar ()
