f07ff1d4ad0e209fe9b8b1879a315ec5921c6375
bleico <bleiderc@gmail.com>
2016-06-21 13:05:06 -0400

    cola base
    1era base para trabajar sobre la cola

--- cola.cpp
+++ cola.cpp
@@ -1,37 +1,117 @@
-
 #include <iostream>
-
+#include <stdlib.h>
 using namespace std;
 
 struct registro
 {
-      int valor;
-      struct registro*sig;
-};
+       int valor;
+       struct registro *sig;       
+}temp;
 
 typedef registro _nodo;
 
-_nodo *crearLista (_nodo *apuntador)
+_nodo *crearCola(_nodo *inicio)
 {
-return (apuntador = NULL);
+      return (inicio = NULL);
 }
 
-bool esListaVacia (_nodo *apuntador)
+bool esColaVacia(_nodo *inicio)
 {
-if(apuntador==NULL)
-return true;
-else 
-return false;
+     if (inicio ==NULL)
+     return true;
+     else
+     return false;
 }
 
-_nodo*insertarEnLista
-(int elemento, _nodo *apuntador)
+_nodo *finDeCola (_nodo *inicio)
 {
-_nodo *registroNuevo, *apuntadorAuxiliar;
-
-registroNuevo = new(crearLista);
-if(registroNuevo !=NULL)
-{
-registroNuevo->valor=elemento;
+      _nodo *aux;
+      aux = inicio;
+      
+      if (esColaVacia(inicio))
+         return NULL;
+      else
+      {
+          while(aux->sig != NULL)
+          aux=aux->sig;
+      }
+      return aux;
 }
-GUSTAVO ES GAY
+
+_nodo *encolar(int elemento, _nodo *inicio)
+{
+      _nodo *nuevoNodo;
+      nuevoNodo = new _nodo;
+      nuevoNodo ->valor=elemento;
+      nuevoNodo ->sig = NULL;
+      
+      if(esColaVacia(inicio))
+      inicio = nuevoNodo;
+      else
+      {
+          _nodo *ultimoNodo;
+          ultimoNodo = finDeCola(inicio);
+          ultimoNodo ->sig = nuevoNodo;
+      }
+      return inicio ;
+}
+
+_nodo *desencolar (_nodo *inicio)
+{
+      _nodo *nodoAEliminar;
+      if(!esColaVacia(inicio))
+      {
+       nodoAEliminar = inicio;
+       inicio = inicio->sig;
+       delete (nodoAEliminar);
+      }
+      return inicio;
+}
+
+/*void imprimir(_nodo *inicio)
+{
+     _nodo *aux;
+     int n,i,arreglo[n];
+     for(i=1;i<=n;i++)
+    {                 
+     cout<<" InicioCola -> ";
+     while (aux!=NULL)
+     {
+     cout<<aux->valor<<" -> ";
+     aux = aux -> sig; 
+     }        
+     cout<<"NULL"<<endl<<endl;                                                                   
+    } 
+}*/
+
+int main()
+{
+    _nodo *inicioCola;
+    _nodo *aux;
+    inicioCola = aux;
+    int n,i,arreglo[n];
+    cout<<" Introduzca cantidad de valores que desea introducir : ";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+    cout<<" Introduzca valor : ";
+    cin>>aux->valor;
+                                       
+    } 
+    inicioCola = crearCola(inicioCola);
+    
+    for(i=1;i<=n;i++)
+    {
+    inicioCola = encolar(inicioCola);
+    inicioCola = desencolar(inicioCola);
+    cout<<" InicioCola -> ";
+     while (aux!=NULL)
+     {
+     cout<<aux->valor<<" -> ";
+     aux = aux -> sig; 
+     }
+     }        
+     cout<<"NULL"<<endl<<endl;  
+    system("pause");
+    return 0;
+}
